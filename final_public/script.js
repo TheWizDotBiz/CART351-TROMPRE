5811ccf31658fbee3b6d28723f6b4b14d2f38b9f
TheWizDotBiz <romprethomas@gmail.com>
2023-12-06 01:07:25 -0500

    damn oopsie

--- final_public/script.js
+++ final_public/script.js
@@ -25,9 +25,7 @@ const loader = new GLTFLoader();
 //import { Socket } from "socket.io";
 //import e from "express";
 
-document.onload(){
-    bckgroundMusic.play();
-};
+
 
 //disables default key use with browser, ie arrowkeys wont move the scrollbar;
 window.addEventListener("keydown", function(e) {
@@ -45,6 +43,7 @@ let wizardList = []; //holds the wizard objects for players.
 let myColorID;
 let wizModelUrls = ['models/wizardRed.glb', 'models/wizardBlue.glb', 'models/wizardGreen.glb', 'models/wizardYellow.glb', 'models/wizard.glb', 'models/wizardPurple.glb', 'models/wizardPurple.glb'];
 let bckgroundMusic = new Audio('sounds/kelethin.mp3');
+document.onload(bckgroundMusic.play());
 let spellSounds = [new Audio('sounds/snd (1).wav'), new Audio('sounds/snd (2).wav'), new Audio('sounds/snd (3).wav'), new Audio('sounds/snd (4).wav'), new Audio('sounds/snd (5).wav'), new Audio('sounds/snd (6).wav'), new Audio('sounds/snd (7).wav')];
 clientSocket.on("connect", function(data){
     console.log("connected");
